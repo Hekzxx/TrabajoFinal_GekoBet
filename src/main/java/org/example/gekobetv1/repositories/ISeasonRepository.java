57d86579a80a85680cea538a8eda2f6c79292c24
ego4men <138946891+ego4men@users.noreply.github.com>
2024-04-25 02:08:39 -0500

    Repository

--- src/main/java/org/example/gekobetv1/repositories/ISeasonRepository.java
+++ src/main/java/org/example/gekobetv1/repositories/ISeasonRepository.java
@@ -2,10 +2,18 @@ package org.example.gekobetv1.repositories;
 
 import org.example.gekobetv1.entities.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface ISeasonRepository extends JpaRepository<Season,Integer> {
-
+    @Query(value = "SELECT t.nameteam\n" +
+            "FROM team t\n" +
+            "INNER JOIN ligue l ON t.id_ligue = l.id\n" +
+            "INNER JOIN season s ON l.id_season = s.id\n" +
+            "WHERE s.year=:año_ingresado",nativeQuery = true)
+    public List<String[]> teamsBySeason(int año_ingresado);
 }
