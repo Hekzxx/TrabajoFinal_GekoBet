b67a28451c17bb80e3bc671905069a54f7587841
CHANDOtheBOSS <alessandro262003@gmail.com>
2024-04-25 00:53:19 -0500

    US_EQUIPOFAVORITOPAIS
    crear repository

--- src/main/java/org/example/gekobetv1/repositories/IFavoriteRepository.java
+++ src/main/java/org/example/gekobetv1/repositories/IFavoriteRepository.java
@@ -2,8 +2,21 @@ package org.example.gekobetv1.repositories;
 
 import org.example.gekobetv1.entities.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface IFavoriteRepository extends JpaRepository<Favorite,Integer> {
+
+    @Query(value = "SELECT t.nameteam, c.namecountry\n" +
+            "FROM favorite f\n" +
+            "JOIN team t ON f.id_team = t.id\n" +
+            "JOIN ligue l ON t.id_ligue = l.id\n" +
+            "JOIN season s ON l.id_season = s.id\n" +
+            "JOIN country c ON s.id_country = c.id\n" +
+            "WHERE f.id_user = :id_usuario", nativeQuery = true)
+    public List<String[]> equiposFavoritosXpais(int id_usuario);
+
 }
