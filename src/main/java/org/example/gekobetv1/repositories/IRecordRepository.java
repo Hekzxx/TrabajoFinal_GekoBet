302348dadf52b366aed261ad7ed1c0f5aa826f39
HugoYupanqui <yupanquito54317@gmail.com>
2024-04-26 14:38:38 -0500

    comit2

--- src/main/java/org/example/gekobetv1/repositories/IRecordRepository.java
+++ src/main/java/org/example/gekobetv1/repositories/IRecordRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface IRecordRepository extends JpaRepository<Record,Integer> {
-    
+
 }
--- src/main/java/org/example/gekobetv1/servicesinterfaces/IFavoriteService.java
+++ src/main/java/org/example/gekobetv1/servicesinterfaces/IFavoriteService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface IFavoriteService {
     public void insert(Favorite favorite);
     public List<Favorite> list();
-    public List<String[]> listaEquipoFavorito(int id_usuario);
+    
     public void delete(int id);
     public Favorite listId(int id);
 }
